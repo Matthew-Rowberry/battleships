a22e362aadefe67851ccf01602d0bc3c5533a0bb
Matthew Rowberry <matthewrowberry16@gmail.com>
2021-12-02 15:54:21 +0000

    added eslintignore

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const a = (): any => {
-  console.log('TURE');
-};
-
 const root = document.getElementById('app');
-
-a();
 
 ReactDOM.render(<h1>Hi</h1>, root);
