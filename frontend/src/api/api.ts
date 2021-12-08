c6ce5cec1ee8e4255d97a5d08e976583b08cdb91
Matthew Rowberry <matthewrowberry16@gmail.com>
2021-12-08 15:09:12 +0000

    updated gitignore

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,32 +1,79 @@
-// type Message = unknown;
-
-// // interface IClient {
-// //   send(message: Message): Promise<>;
-// // }
-
-// const createApiClient = async (wsURL) => {
-//   return {};
-// };
-
-// const client = await createApiClient('ws://wasdasd');
-
-// try {
-//   const response = await client.send({
-//     type: 'SEND_MESSAGE',
-//     payload: { content: 'hi' },
-//   });
-// } catch (e) {
-//   console.log(e);
-// }
-
-// client.subscribe('RECIEVE_MESSAGE', (m) => {
-//   console.log('based');
-// });
-
+interface ISubscribers {
+  [key: string]: ((value: unknown) => void)[];
+}
 class BattleshipClient {
-  public async send(message: Message): Promise<void> {}
+  private socket: WebSocket;
+  private subscribers: ISubscribers;
+  private requests: Record<number, { rs: () => void; rj: () => void }>;
+  private requestId: number;
 
-  public subscribe(message: string, callback: Function) {}
+  constructor(socket: WebSocket) {
+    this.requestId = 0;
+    this.socket = socket;
+    this.socket.onmessage = this.handleMessage;
+    this.subscribers = {};
+    this.requests = {};
+  }
+
+  public async send(message: object): Promise<unknown> {
+    this.socket.send(
+      JSON.stringify({
+        ...message,
+        ref: this.requestId,
+      })
+    );
+
+    const promise = new Promise<void>((resolve, reject) => {
+      this.requests[this.requestId] = { rs: resolve, rj: reject };
+    });
+
+    this.requestId++;
+
+    return promise;
+  }
+
+  public subscribe(message: string, callback: () => void) {
+    // If this message hasnt been subscribed before add in new list
+    if (!this.subscribers[message]) {
+      this.subscribers[message] = [];
+    }
+
+    // Add callback to list
+    this.subscribers[message].push(callback);
+  }
+
+  public unsubscribe(message: string, callback: () => void) {
+    this.subscribers[message] = this.subscribers[message].filter(
+      (cb) => cb !== callback
+    );
+  }
+
+  private handleMessage = (plainMessage: MessageEvent<any>): void => {
+    const message = JSON.parse(plainMessage.data);
+
+    // Alert subscribers that subscribed with type `message.type`
+    if (this.subscribers[message.type]) {
+      for (const subscriber of this.subscribers[message.type]) {
+        subscriber(message.payload);
+      }
+    }
+
+    // Resolve any callers who used send()
+    if (message.ref) {
+      if (message.type === 'ACK') {
+        this.requests[message.ref].rs();
+      }
+
+      if (message.type === 'ERROR') {
+        this.requests[message.ref].rj();
+      }
+
+      delete this.requests[message.ref];
+    }
+  };
 }
 
-const client = new BattleshipClient();
+const ws = new WebSocket('hi');
+// TODO: we should wait for the websocket to be connected fully before anything else
+
+const client = new BattleshipClient(ws);
